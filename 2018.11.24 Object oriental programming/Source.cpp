1fd85f95d022f4852e4daa564d68d3d2aaefb48e
Dima Zeland <onenewinvest@gmail.com>
2018-11-24 17:18:29 +0200

    Update Source.cpp

--- 2018.11.24 Object oriental programming/Source.cpp
+++ 2018.11.24 Object oriental programming/Source.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
 #include"Person.h"
 
+Person Global;
 
 int main() {
-	Person Dima;
-	Dima.Set_Data();
-	Dima.Show_Data();
-	Dima.Input_Birth();
-	std::cout << "\nBirth: " << Dima.Get_Birth() << '\n';
-	Dima.Input_Name();
-	std::cout << "\nName: " << Dima.Get_Name() << '\n';
-	Dima.Input_Hobby();
-	std::cout << "\nHobby: " << Dima.Get_Hobby() << '\n';
+	
+	Person* pDynamic = new Person;
+	{
+		Person Dima;
+		Dima.Set_Data();
+		Dima.Show_Data();
+		Dima.Input_Birth();
+		std::cout << "\nBirth: " << Dima.Get_Birth() << '\n';
+		Dima.Input_Name();
+		std::cout << "\nName: " << Dima.Get_Name() << '\n';
+		Dima.Input_Hobby();
+		std::cout << "\nHobby: " << Dima.Get_Hobby() << '\n';
+	}
 
-
-
+	delete pDynamic;
+	pDynamic = nullptr;
 	system("pause");
 	return 0;
 }
